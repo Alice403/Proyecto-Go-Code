5c1e2c3ce69818d8a5cb19cd2aade42e58cf0077
Alice403 <alejandra403.arciniegas@hotmail.es>
2021-09-29 18:12:24 -0500

    cambios interfaz inicio de sesion

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+function msg() {
+    var user = document.getElementById('UserInput').value;
+    var pwd = document.getElementById('UserPassword').value;
+
+    alert(`Hola ${user} tu password es ${pwd}`);
+}
