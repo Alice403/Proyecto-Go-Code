450430c544a795ef41752c6e35ce0f4ddc70cd99
xjuandiego28 <juandiegosamp@hotmail.com>
2021-09-30 19:11:30 -0500

    revirtiendo a un commit anterior

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-function msg() {
-    var user = document.getElementById('UserInput').value;
-    var pwd = document.getElementById('UserPassword').value;
-
-    alert(`Hola ${user} tu password es ${pwd}`);
-}
