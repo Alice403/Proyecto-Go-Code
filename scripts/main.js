c4ed23bf2c97a530babc903bf765fe3eec3d9c36
Alice403 <alejandra403.arciniegas@hotmail.es>
2021-10-01 10:38:03 -0500

    actualización autenticación y registro

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,11 @@ function msg() {
 
     alert(`Hola ${user} tu password es ${pwd}`);
 }
+
+function msg1() {
+    var NewUser = document.getElementById('NewUser').value;
+    var NewPwd = document.getElementById('NewPassword').value;
+    var email = document.getElementById('NewEmail').value;
+
+    alert(`Hola, ahora estás registrado como ${NewUser} , tu password es ${NewPwd} y tu correo es ${email}`);
+}
