182be84221acb81f65d68c910db79abc5dc82bc2
xjuandiego28 <juandiegosamp@hotmail.com>
2021-10-07 00:34:25 -0500

    Preparado el proyecto React para empezar con la v1 de la aplicación - v0

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div> Si te aparece esto es porque la página funciona, buscame en App.js 
+      o App.jsx
+    </div>
+  );
+}
+
+export default App;
