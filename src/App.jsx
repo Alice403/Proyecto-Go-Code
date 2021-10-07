579f62127dffcb2b35a0aa871414fd21c0dd596d
Julio Pinzon <je.pinzon@gicolombia.com>
2021-10-07 14:23:54 -0500

    Menu de inici en React

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import './App.css';
+import './styles/styles.css';
+import logoCuadernia from './media/logo_cuadernia.png'
 
 function App() {
   return (
-    <div> Si te aparece esto es porque la página funciona, buscame en App.js 
-      o App.jsx
+    <div className='App'>     
+      <section className="botones">
+      <div className="contenedor-logo">
+        <img className="logo" src= {logoCuadernia} alt="cuadernia"/>
+    </div>
+    <ul>
+        <div className="containerMainButton">
+            <a href="admin_ventas.html"><button  className="mainButton button1">Administrar ventas</button></a>
+        </div>
+        <div className="containerMainButton">
+            <a href="admin_ventas.html"><button  className="mainButton button2">Administrar productos</button></a>
+        </div>
+        <div className="containerMainButton">
+            <a href="gestion_usuarios.html"><button  className="mainButton button3">Administrar usuarios</button></a>
+        </div>
+    </ul>
+      </section>
     </div>
   );
 }
