91b585acc3834024aeb277210f49f395eb36ac6e
xjuandiego28 <juandiegosamp@hotmail.com>
2021-10-07 19:08:47 -0500

    corregido error de menu admin

--- src/pages/Administracion/menu_inicio.jsx
+++ src/pages/Administracion/menu_inicio.jsx
@@ -3,7 +3,22 @@ import React from 'react'
 const menu_inicio = () => {
   return (
     <div>
-      Menu de inicio
+      <section className="botones">
+        <div className="contenedor-logo">
+          <img className="logo" alt="Logo de Cuadernia"/>
+        </div>
+        <ul>
+          <div className="containerMainButton">
+              <a href="admin_ventas.html"><button  className="mainButton button1">Administrar ventas</button></a>
+          </div>
+          <div className="containerMainButton">
+              <a href="admin_ventas.html"><button  className="mainButton button2">Administrar productos</button></a>
+          </div>
+          <div className="containerMainButton">
+              <a href="gestion_usuarios.html"><button  className="mainButton button3">Administrar usuarios</button></a>
+          </div>
+        </ul>
+      </section>
     </div>
   )
 }
