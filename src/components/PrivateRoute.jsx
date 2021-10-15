240ed098b4cff8e0e3c7b090f16f7f841aa33fb8
Alice403 <alejandra403.arciniegas@hotmail.es>
2021-10-15 13:03:12 -0500

    cambios de mayusculas a minusculas

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -4,8 +4,8 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { Link } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
-  const { isAuthenticated, isLoading } = useAuth0();
-
+  const {isAuthenticated, isLoading} = useAuth0();
+  console.log(isAuthenticated);
   if (isLoading) return <div>Cargando...</div>;
 
   return isAuthenticated ? (
