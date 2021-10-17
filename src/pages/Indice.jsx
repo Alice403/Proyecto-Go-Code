8ba294dc4cd6a642060c2115b6aefa8e4e62f6c1
xjuandiego28 <juandiegosamp@hotmail.com>
2021-10-17 08:50:00 -0500

    Corregidos botones inicio y agregada fuente a pantalla carga

--- src/pages/Indice.jsx
+++ src/pages/Indice.jsx
@@ -20,89 +20,78 @@ const Indice = () => {
   
   return (
     <div className = 'cuerpoIndice'>
-         
-            <section className="panelLog">
 
-            <i  className="fas fa-shopping-cart"></i>
-            <button className = "botonIn"onClick= {() => loginWithRedirect()}>Iniciar sesión</button>
-            <button className = "botonOut" onClick={() => cerrarSesion()}>Cerrar sesión</button>
+      <section className="panelLog">
+        <i  className="fas fa-shopping-cart"></i>
+        <button className = "botonIn"onClick= {() => loginWithRedirect()}>Iniciar sesión</button>
+        <button className = "botonOut" onClick={() => cerrarSesion()}>Cerrar sesión</button>
+      </section>
 
-            </section>
+      <section>
+        <div className="contenedorNavBar">
+          <div></div>
+          <div></div>
+          <div>INICIO</div>
+          <div>TIENDA</div>
+          <div>SERVICIOS</div>
+          <div>CONTACTO</div>
+          <div></div>
+          <div></div>
+        </div>
+      </section>
 
-            <section>
-                <div className="contenedorNavBar">
-                    <div></div>
-                    <div></div>
-                    <div>INICIO</div>
-                    <div>TIENDA</div>
-                    <div>SERVICIOS</div>
-                    <div>CONTACTO</div>
-                    <div></div>
-                    <div></div>
-                </div>
+      <section>
+        <div className="contenedorLogoPrincipal">
+          <div className="contenedorTienda">
+            <ul>
+              <li><h1>TIENDA</h1></li>
+              <li>¿Quieres comprar?</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li> <img className = "tituloCuadernia" src = {Cuadernia} alt="LetreroCuadernia"></img></li>
+            </ul>
+          </div>
+          <div>
+          <img className = "logoCuadernia" src = {Logo} alt="Logo Cuadernia"></img>
+          </div>
+        </div>
+      </section>
 
-            </section>
+      <h2 className = 'textoIntermedio'>Escribe tus sueños y hazlos realidad</h2>
+      <section className="contenedorCuaderno">
+        <div>
+          <ul className = 'barraOpciones'>
+            <li><button className="botonTipoCuaderno botonCuaderno">Tipo de cuaderno</button></li>
+            <li><button className="botonRayado botonCuaderno">Tipo de Rayado</button></li>
+            <li><button className="botonPortada botonCuaderno">Portada</button></li>
+            <li><button className="botonNumero botonCuaderno">Numero de Hojas</button></li>
+            <li><button className="botonPrecio botonCuaderno">Precio</button></li>
+          </ul>
+        </div>
 
-            <section>
-              <div className="contenedorLogoPrincipal">
-                <div className="contenedorTienda">
-                  <ul>
-                    <li><h1>TIENDA</h1></li>
-                    <li>¿Quieres comprar?</li>
-                  </ul>
-                </div>
+        <div>
+          <ul>
+            <li><h4>CUADERNO PERSONALIZADO 1</h4></li>
+            <li><img className = "CuadernoInicio" src = {Cuaderno} alt="Cuaderno1"></img></li>
+          </ul>
+        </div>
 
-                <div>
-                 <ul>
-                    <li> <img className = "tituloCuadernia" src = {Cuadernia} alt="LetreroCuadernia"></img></li>
-                    <li><h2>Escribe tus sueños y hazlos realidad</h2></li>
-                  </ul>
-                </div>
-
-                <div>
-                <img className = "logoCuadernia" src = {Logo} alt="Logo Cuadernia"></img>
-                </div>
-
-              </div>
-
-            </section>
-
-            <section className="contenedorCuaderno">
-            <div>
-              <ul>
-                <li><button className="botonTipoCuaderno">Tipo de cuaderno</button></li>
-                <li><button className="botonRayado">Tipo de Rayado</button></li>
-                <li><button className="botonPortada">Portada</button></li>
-                <li><button className="botonNumero">Numero de Hojas</button></li>
-                <li><button className="botonPrecio">Precio</button></li>
-              </ul>
-            </div>
-
-            <div>
-              <ul>
-                <li><h4>CUADERNO PERSONALIZADO 1</h4></li>
-                <li><img className = "CuadernoInicio" src = {Cuaderno} alt="Cuaderno1"></img></li>
-              </ul>
-            </div>
-
-            <div>
-              <ul>
-                <li><h4>CUADERNO PERSONALIZADO 2</h4></li>
-                <li><img className = "CuadernoInicio" src = {Cuaderno2} alt="Cuaderno2"></img></li>
-              </ul>
-            </div>
-            
-            <div>
-              <ul>
-                <li><h4>CUADERNO PERSONALIZADO 3</h4></li>
-                <li><img className = "CuadernoInicio" src = {Cuaderno3} alt="Cuaderno3"></img></li>
-              </ul>
-            </div>
-            </section>
-
-
-
-
+        <div>
+          <ul>
+            <li><h4>CUADERNO PERSONALIZADO 2</h4></li>
+            <li><img className = "CuadernoInicio" src = {Cuaderno2} alt="Cuaderno2"></img></li>
+          </ul>
+        </div>
+        
+        <div>
+          <ul>
+            <li><h4>CUADERNO PERSONALIZADO 3</h4></li>
+            <li><img className = "CuadernoInicio" src = {Cuaderno3} alt="Cuaderno3"></img></li>
+          </ul>
+        </div>
+      </section>
     </div>
     
   )
